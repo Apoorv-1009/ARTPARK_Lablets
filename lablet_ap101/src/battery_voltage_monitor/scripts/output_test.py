8ecc1ea27fe1ab5335f4237a5f4691f2d9f08f06
Apoorv Thapliyal <57452076+Apoorv-1009@users.noreply.github.com>
2021-06-17 00:11:35 +0530

    Delete lablet_ap101/src directory

--- lablet_ap101/src/battery_voltage_monitor/scripts/output_test.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-import rospy as ry
-
-if __name__ == "__main__":
-    ry.init_node("output_test", anonymous=False)
-    ry.loginfo("This is an output test!")
-    
-    ry.spin()
--- lablet_ap101/src/ultrasound_sensor_driver/scripts/ultrasound_sensor_driver.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-import rospy
-
-if __name__ == "__main__":
-    rospy.init_node("ultrasound_sensor_driver", anonymous=False)
-    rospy.loginfo("I process ultrasound sensor data")
-
-    rospy.spin()
-
